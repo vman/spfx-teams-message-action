820f5a610f58ae59bacf09d2e6be8ae95b670362
vman <vardhaman.rd@gmail.com>
2020-10-03 17:51:36 +0100

    first draft of demo

--- src/webparts/teamsMessageAction/TeamsMessageActionWebPart.ts
+++ src/webparts/teamsMessageAction/TeamsMessageActionWebPart.ts
@@ -2,6 +2,7 @@ import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import { AadHttpClientFactory, AadHttpClient, HttpClientResponse } from '@microsoft/sp-http';
 
 import styles from './TeamsMessageActionWebPart.module.scss';
+import { UrlQueryParameterCollection } from '@microsoft/sp-core-library';
 
 export interface ITeamsMessageActionWebPartProps {
   description: string;
@@ -21,16 +22,23 @@ export default class TeamsMessageActionWebPart extends BaseClientSideWebPart<ITe
     const chatId = teamsContext.chatId;
 
     //As of SPFx 1.11, the teams js types bundled with the SharePoint Framework don't contain the parentMessageId yet.
-    const parentMessageId = (teamsContext as any).parentMessageId;
+    let messageId = (teamsContext as any).parentMessageId;
 
 
-    this.getTeamsMessageDetails(this.context.aadHttpClientFactory, teamId, channelId, chatId, parentMessageId);
+    const queryStringParams = new UrlQueryParameterCollection(window.location.href);
+    if (!messageId) {
+      messageId = queryStringParams.getValue("augMessageId");
+    }
+
+    const replyToId = queryStringParams.getValue("augReplyToId");
+
+    this.getTeamsMessageDetails(this.context.aadHttpClientFactory, teamId, channelId, chatId, messageId, replyToId);
 
     this.domElement.innerHTML = `
-      <div class="${ styles.teamsMessageAction}">
-        <div class="${ styles.container}">
-          <div class="${ styles.row}">
-              <span class="${ styles.title}">SPFx web part inside Teams message action!</span>
+      <div class="${styles.teamsMessageAction}">
+        <div class="${styles.container}">
+          <div class="${styles.row}">
+              <span class="${styles.title}">SPFx web part inside Teams message action!</span>
           </div>
             <div class="${styles.row}">
               <span class="${styles.subTitle}">Team Id (O365 Group Id):</span>
@@ -45,18 +53,18 @@ export default class TeamsMessageActionWebPart extends BaseClientSideWebPart<ITe
               <div id="chatIdContainer">${chatId}</div>
             </div>
             <div class="${styles.row}">
-              <span class="${styles.subTitle}">Parent Message Id:</span>
-              <div id="parentMessageIdContainer">${parentMessageId}</div>
+              <span class="${styles.subTitle}">Message Id:</span>
+              <div id="parentMessageIdContainer">${messageId ? messageId : ""}</div>
             </div>
             <div class="${styles.row}">
-              <span class="${styles.subTitle}">Parent Message Body:</span>
+              <span class="${styles.subTitle}">Message Body:</span>
               <div id="parentMessageBodyContainer"></div>
             </div>
         </div>
       </div>`;
   }
 
-  private async getTeamsMessageDetails(aadHttpClientFactory: AadHttpClientFactory, teamId: string, channelId: string, chatId: string, parentMessageId: string) {
+  private async getTeamsMessageDetails(aadHttpClientFactory: AadHttpClientFactory, teamId: string, channelId: string, chatId: string, messageId: string, replyToId: string) {
 
     const _aadHttpClient: AadHttpClient = await aadHttpClientFactory.getClient("https://graph.microsoft.com");
 
@@ -64,12 +72,20 @@ export default class TeamsMessageActionWebPart extends BaseClientSideWebPart<ITe
 
     //If teamId and channelId are present, it means the message action was invoked in a teams channel message
     if (teamId && channelId) {
-      response = await _aadHttpClient.get(`https://graph.microsoft.com/beta/teams/${teamId}/channels/${channelId}/messages/${parentMessageId}`, AadHttpClient.configurations.v1);
+
+      //If it's a reply to a message in a teams conversation
+      if (replyToId) {
+        response = await _aadHttpClient.get(`https://graph.microsoft.com//beta/teams/${teamId}/channels/${channelId}/messages/${replyToId}/replies/${messageId}`, AadHttpClient.configurations.v1);
+
+      }
+      else {
+        response = await _aadHttpClient.get(`https://graph.microsoft.com/beta/teams/${teamId}/channels/${channelId}/messages/${messageId}`, AadHttpClient.configurations.v1);
+      }
     }
     else if (chatId) {
-    //If chatId is present, it means the message action was invoked in either a teams 1:1 chat or a group chat
+      //If chatId is present, it means the message action was invoked in either a teams 1:1 chat or a group chat
 
-      response = await _aadHttpClient.get(`https://graph.microsoft.com/beta/chats/${chatId}/messages/${parentMessageId}`, AadHttpClient.configurations.v1);
+      response = await _aadHttpClient.get(`https://graph.microsoft.com/beta/chats/${chatId}/messages/${messageId}`, AadHttpClient.configurations.v1);
 
     }
 
